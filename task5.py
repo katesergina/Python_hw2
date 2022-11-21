23df2081640294f8d32f94a7b9da41f36e2b1f8e
Ekaterina Sergina <weenfry@mail.ru>
2022-11-21 12:24:19 +0300

    Edded task 5

---
+++ task5.py
@@ -0,0 +1,16 @@
+# 5) Реализуйте алгоритм перемешивания списка.
+
+import random
+
+def shuffle_list(lst):
+    for i in range(len(lst) - 1, 0, -1):
+        j = random.randint(0, i + 1)
+        lst[i], lst[j] = lst[j], lst[i]
+
+lst = [random.randint(0,10) for i in range(random.randint(5,20))]
+print(lst)
+shuffle_list(lst)
+print(lst)
+
+
+
